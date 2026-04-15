92d2d4cb505a187df45007184345da25be786548
Darel.Grace Kamen <darel.grace-kamen@stu.ocsb.ca>
2026-04-14 22:36:52 -0400

    i made a moving pixel YAY

--- main.ts
+++ main.ts
@@ -1,8 +1,43 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Grace
+ * Created on: April 2026
+ * This program will make a moving pixel 
 */
 
-basic.showString('Hello, World!')
+// variables
+let sprite: game.LedSprite = null
+let sideCounter = 0
+let stepCounter = 0
+
+// setup
+basic.showIcon(IconNames.Happy)
+basic.pause(2000)
+basic.clearScreen()
+
+// 
+input.onButtonPressed(Button.A, function () {
+    // setup
+    basic.clearScreen()
+    sprite = game.createSprite(0, 0)
+    sideCounter = 0
+
+    // 
+    while (sideCounter < 4) {
+        stepCounter = 0
+
+        // 
+        while (stepCounter < 4) {
+            sprite.move(1)
+            stepCounter = stepCounter + 1
+            basic.pause(200)
+        }
+
+        // turn 90 degrees at the corner
+        sprite.turn(Direction.Right, 90)
+        sideCounter = sideCounter + 1
+    }
+
+    sprite.delete()
+    basic.showIcon(IconNames.Happy)
+})
